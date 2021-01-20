1656a9dbf1143f5471f996c0a8e207b2bd0ba104
LuisLota <louis.miguel.05@gmail.com>
2021-01-20 17:04:06 +0000

    adding ingredients dynamic way with the state of each ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,29 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
+    // transform object with key vaules into array[cheese,meat,bacon]
+    // depedent of how many values for each index of value par create the arr 
+    // for each value par of ingredient-value loop into ingredients component with value name and type
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_,i) => {
+            return  <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+    })
+    // join to only array of objects [{key : cheese},{ key : bacon}]
+    .reduce((arr,el) => {
+        return arr.concat(el)
+    },[]);
+
+    if(transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please start adding ingredients !</p>
+    }
+    console.log(transformedIngredients);
+
     return(
         <div className={classes.Burger}>
         <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="meat"/>
+        {transformedIngredients}
         <BurgerIngredient type="bread-bottom"/>
 
         </div>
