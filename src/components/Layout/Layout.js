1355abb51dc30774e5082aa50de6f816a4e43370
LuisLota <louis.miguel.05@gmail.com>
2021-01-20 15:54:34 +0000

    first configuration for component & containers

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Aux from '../../hoc/Auxillity';
+import classes from './Layout.css';
+
+const layout = (props) => (
+  <Aux>
+    <div>Toolbar,SideDrawer,backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
+);
+
+export default layout;
