ee849015d740e4af23f732d3d9453f03bebe0ead
LuisLota <louis.miguel.05@gmail.com>
2021-01-27 21:02:23 +0000

    refactor and optimize app

--- src/hoc/Layout/Navigation/NavigationItems/NavigationItems.js
+++ src/hoc/Layout/Navigation/NavigationItems/NavigationItems.js
@@ -3,18 +3,14 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 import classes from '../NavigationItems/NavigationItems.css'
 
-const navigationItems = (props) => {
-  return  (
-    <ul className={classes.NavigationItems}>
-     <NavigationItem link="/">Burger Builder</NavigationItem>
-     {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null } 
-     { !props.isAuthenticated ? 
-    <NavigationItem link="/auth">Authenticate</NavigationItem>
-    : 
-    <NavigationItem link="/logout">logout</NavigationItem>
-
-    } 
-    </ul>
-)};
+const navigationItems = ( props ) => (
+  <ul className={classes.NavigationItems}>
+      <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+      {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
+      {!props.isAuthenticated
+          ? <NavigationItem link="/auth">Authenticate</NavigationItem>
+          : <NavigationItem link="/logout">Logout</NavigationItem>}
+  </ul>
+);
 
 export default navigationItems;
