f40a5ae3c124569f6b322e34e6a90e94f27f8767
LuisLota <louis.miguel.05@gmail.com>
2021-01-27 18:12:04 +0000

    filter user info

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
-
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -15,28 +14,40 @@ class App extends Component {
 		this.props.onTryAutoSignup();
 	}
 	render() {
+		let routes = (
+			<Switch>
+				<Route path='/auth' component={Auth} />
+				<Route path='/' exact component={BurgerBuilder} />
+				<Redirect to='/' />
+			</Switch>
+		);
+
+		if (this.props.isAuthenticated) {
+			<Switch>
+				<Route path='/checkout' component={Checkout} />
+				<Route path='/orders' component={Orders} />
+				<Route path='/logout' component={Logout} />
+				<Route path='/' exact component={BurgerBuilder} />
+				<Redirect to='/' />
+			</Switch>;
+		}
 		return (
 			<div>
-				<Layout>
-					<Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
-          <Route path="/logout" component={Logout} />
-
-          <Route path="/" exact component={BurgerBuilder} />
-
-
-					</Switch>
-				</Layout>
+				<Layout>{routes}</Layout>
 			</div>
 		);
 	}
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = (state) => {
 	return {
-		onTryAutoSignup : () => dispatch(actions.authCheckState())
-	}
-}
-export default  withRouter(connect(null,mapDispatchToProps)(App));
+		isAuthenticated: state.auth.token !== null
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onTryAutoSignup: () => dispatch(actions.authCheckState())
+	};
+};
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
